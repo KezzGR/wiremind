b6acb22940b9fdca3bf0a183e86d472389dabbf7
Nikita Krasnyh <nik2005kras@gmail.com>
2026-09-04 15:25:42 +0300

    add TCP message exchange

--- Wiremind.Desktop/Program.cs
+++ Wiremind.Desktop/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System.Net;
 using System.Net.Sockets;
+using System.Text;
 
 TcpClient client = new();
 
@@ -8,3 +9,14 @@ Console.WriteLine("Connecting...");
 client.Connect(IPAddress.Loopback, 5000);
 
 Console.WriteLine("Connected!");
+
+
+NetworkStream stream = client.GetStream();
+
+byte[] buffer = new byte[1024];
+
+int bytesRead = stream.Read(buffer);
+
+string message = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+
+System.Console.WriteLine($"Received message: {message}.");
--- Wiremind.Simulator/Program.cs
+++ Wiremind.Simulator/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System.Net;
 using System.Net.Sockets;
+using System.Text;
 
 TcpListener listener = new(IPAddress.Loopback, 5000);
 
@@ -10,3 +11,13 @@ Console.WriteLine("Waiting for connection...");
 TcpClient client = listener.AcceptTcpClient();
 
 Console.WriteLine("Client connected!");
+
+NetworkStream stream = client.GetStream();
+
+string message = "457";
+
+byte[] data = Encoding.UTF8.GetBytes(message);
+
+stream.Write(data);
+
+Console.WriteLine($"Send message: {message}.");
