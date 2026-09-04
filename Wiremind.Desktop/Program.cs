e184b3bb7e45e126ca0ec01553780b847da71f9f
Nikita Krasnyh <nik2005kras@gmail.com>
2026-09-04 14:42:04 +0300

    establish TCP connection between simulator and desktop

--- Wiremind.Desktop/Program.cs
+++ Wiremind.Desktop/Program.cs
@@ -1,1 +1,10 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System.Net;
+using System.Net.Sockets;
+
+TcpClient client = new();
+
+Console.WriteLine("Connecting...");
+
+client.Connect(IPAddress.Loopback, 5000);
+
+Console.WriteLine("Connected!");
--- Wiremind.Simulator/Program.cs
+++ Wiremind.Simulator/Program.cs
@@ -1,1 +1,12 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System.Net;
+using System.Net.Sockets;
+
+TcpListener listener = new(IPAddress.Loopback, 5000);
+
+listener.Start();
+
+Console.WriteLine("Waiting for connection...");
+
+TcpClient client = listener.AcceptTcpClient();
+
+Console.WriteLine("Client connected!");
